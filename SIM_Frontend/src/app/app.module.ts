01282d151da46e7232d61fcef3fcde0f70de7c2e
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-08-07 23:41:23 +0200

    Update app.module.ts

--- SIM_Frontend/src/app/app.module.ts
+++ SIM_Frontend/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NavUserComponent } from './components/nav-user/nav-user.component';
+import { IfAuthenticatedDirective } from './directives/if-authenticated.directive';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { TodosComponent } from './pages/todos/todos.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavUserComponent,
+    IfAuthenticatedDirective,
+    TodosComponent,
+    TodoCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
