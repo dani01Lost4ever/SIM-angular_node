f8012a56486ffbf1180980aa71b89b96be4a9953
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-08-08 01:14:03 +0200

    Update todos.service.ts

--- SIM_Frontend/src/app/services/todos.service.ts
+++ SIM_Frontend/src/app/services/todos.service.ts
@@ -7,14 +7,28 @@ import { Observable, map } from 'rxjs';
   providedIn: 'root',
 })
 export class TodosService {
+  private todosWithFilter: Todo[] = []; // Store the filtered todos
   constructor(private http: HttpClient) {}
 
-  list() {
-    return this.http.get<Todo[]>('/api/todos/');
+  list(showCompleted: boolean): Observable<Todo[]> {
+    console.log('List_Refreshed');
+    return this.http.get<Todo[]>(`/api/todos/?showCompleted=${showCompleted}`);
   }
 
-  count(): Observable<number> {
-    // Simulate counting locally based on the filters
-    return this.list().pipe(map((todo) => todo.length));
+  count(showCompleted: boolean): Observable<number> {
+    console.log('Count_Refreshed');
+    return this.list(showCompleted).pipe(map((todos) => todos.length));
   }
+
+  //OLD
+  // list(showCompleted: boolean) {
+  //   if (showCompleted == false) return this.http.get<Todo[]>('/api/todos/');
+  //   else
+  //     return this.http.get<Todo[]>(`/api/todos/?showCompleted=${showCompleted}`);
+  // }
+
+  // count(): Observable<number> {
+  //   // Simulate counting locally based on the filters
+  //   return this.list().pipe(map((todo) => todo.length));
+  // }
 }
