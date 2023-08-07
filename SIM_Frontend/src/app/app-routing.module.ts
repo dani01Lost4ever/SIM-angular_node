b2aed37e1131b4e96230cf39bdc097c0a610a482
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-08-07 23:41:22 +0200

    Update app-routing.module.ts

--- SIM_Frontend/src/app/app-routing.module.ts
+++ SIM_Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { authGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'todos',
+    component: TodosComponent,
+    canActivate: [authGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
