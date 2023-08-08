8c24644f5b76b043a3ff1194f5043954ca0dd8c8
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-08-09 01:13:43 +0200

    Update todos.service.ts

--- SIM_Frontend/src/app/services/todos.service.ts
+++ SIM_Frontend/src/app/services/todos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Todo } from '../interfaces/todos';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
@@ -10,14 +11,25 @@ export class TodosService {
   private todosWithFilter: Todo[] = []; // Store the filtered todos
   constructor(private http: HttpClient) {}
 
-  list(showCompleted: boolean): Observable<Todo[]> {
+  list(showCompleted: boolean, showExpired: boolean): Observable<Todo[]> {
     console.log('List_Refreshed');
-    return this.http.get<Todo[]>(`/api/todos/?showCompleted=${showCompleted}`);
+    return this.http
+      .get<Todo[]>(`/api/todos/?showCompleted=${showCompleted}`)
+      .pipe(
+        map((todos: Todo[]) => {
+          if (!showExpired) {
+            return todos.filter((todo) => !todo.expired);
+          }
+          return todos;
+        })
+      );
   }
 
-  count(showCompleted: boolean): Observable<number> {
+  count(showCompleted: boolean, showExpired: boolean): Observable<number> {
     console.log('Count_Refreshed');
-    return this.list(showCompleted).pipe(map((todos) => todos.length));
+    return this.list(showCompleted, showExpired).pipe(
+      map((todos) => todos.length)
+    );
   }
 
   //OLD
@@ -31,4 +43,14 @@ export class TodosService {
   //   // Simulate counting locally based on the filters
   //   return this.list().pipe(map((todo) => todo.length));
   // }
+
+  userList(): Observable<User[]> {
+    return this.http.get<User[]>('/api/users');
+  }
+
+  assignTo(todoId: string, userId: string): Observable<object> {
+    return this.http.post(`/api/todos/${todoId}/assignTo`, {
+      userId: userId,
+    });
+  }
 }
