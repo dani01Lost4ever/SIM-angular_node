0b0a2d8c385c9e862e6ab04d94326b4175464ae6
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-08-09 01:14:05 +0200

    Update todos.service.ts

--- src/api/todos/todos.service.ts
+++ src/api/todos/todos.service.ts
@@ -12,19 +12,42 @@ export class TodosService {
   async find(userId: string, showCompleted: boolean): Promise<Todo[]>;
   async find(userId: string): Promise<Todo[]>;
   async find(userId: string, showCompleted: boolean = false) {
+    //OLD
+    // const now = new Date();
+    // const filter: any = {
+    //   $or: [{ createdBy: userId }, { assignedTo: userId }],
+    // };
+
+    // if (!showCompleted) {
+    //   filter.completed = false;
+    // }
+
+    // const list = await TodosModel.find(filter)
+    //   .populate("createdBy assignedTo")
+    //   .sort({ dueDate: 1, createdAt: 1 });
+    // return list;
     const now = new Date();
     const filter: any = {
       $or: [{ createdBy: userId }, { assignedTo: userId }],
     };
-
     if (!showCompleted) {
       filter.completed = false;
     }
-
-    const list = await TodosModel.find(filter)
+    const listWithDueDate = await TodosModel.find({
+      ...filter,
+      dueDate: { $exists: true, $ne: null },
+    })
       .populate("createdBy assignedTo")
       .sort({ dueDate: 1, createdAt: 1 });
-    return list;
+
+    const listWithoutDueDate = await TodosModel.find({
+      ...filter,
+      dueDate: { $exists: false },
+    })
+      .populate("createdBy assignedTo")
+      .sort({ createdAt: 1 });
+
+    return [...listWithDueDate, ...listWithoutDueDate];
   }
 
   async add(todo: Todo, createdBy: string): Promise<Todo> {
