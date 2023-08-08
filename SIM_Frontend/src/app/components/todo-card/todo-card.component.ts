e6aaea9e77337badb9915b0a8878927527681e33
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-08-09 01:13:42 +0200

    Update todo-card.component.ts

--- SIM_Frontend/src/app/components/todo-card/todo-card.component.ts
+++ SIM_Frontend/src/app/components/todo-card/todo-card.component.ts
@@ -1,11 +1,62 @@
-import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from 'src/app/interfaces/todos';
-
+import { MatDialog } from '@angular/material/dialog';
+import { AssignedToModalComponent } from '../assigned-to-modal/assigned-to-modal.component';
+import { User } from 'src/app/interfaces/user';
+import { TodosService } from 'src/app/services/todos.service';
 @Component({
   selector: 'app-todo-card',
   templateUrl: './todo-card.component.html',
   styleUrls: ['./todo-card.component.css'],
+  providers: [DatePipe],
 })
 export class TodoCardComponent {
   @Input() todo!: Todo;
+  @Output() refreshList = new EventEmitter<void>();
+  constructor(
+    private datePipe: DatePipe,
+    private dialog: MatDialog,
+    private Srv: TodosService
+  ) {}
+
+  isDueDateClose(dueDateString: string | Date): boolean {
+    const dueDate = new Date(dueDateString);
+    if (isNaN(dueDate.getTime())) {
+      throw new Error('Invalid date format');
+    }
+    const currentDate = new Date();
+    const timeDifference = dueDate.getTime() - currentDate.getTime();
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
+    // console.log(
+    //   'NotFormattedDueDate_',
+    //   dueDateString,
+    //   'DueDate_',
+    //   dueDate,
+    //   'Difference_',
+    //   daysDifference,
+    //   'Value_',
+    //   daysDifference <= 30,
+    //   'TODO_',
+    //   this.todo.title
+    // );
+    return daysDifference <= 50;
+  }
+
+  openUserModal() {
+    const dialogRef = this.dialog.open(AssignedToModalComponent);
+
+    dialogRef.afterClosed().subscribe((selectedUser: User) => {
+      console.log('SelectedUser_', selectedUser);
+      this.Srv.assignTo(this.todo.id!, selectedUser.id!).subscribe(
+        (response) => {
+          console.log('Response: ', response);
+          this.refreshList.emit();
+        },
+        (error) => {
+          console.log('Error: ', error);
+        }
+      );
+    });
+  }
 }
