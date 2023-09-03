d7f1e2bfa7b74761ff1bdfcc972953c8636d3df0
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-03 17:28:34 +0200

    Update routes.ts

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import userRouter from "./users/user.router";
 import authRouter from "./auth/auth.router";
 import todosRouter from "./todos/todos.router";
+import generatorRouter from "./generator/generator.router";
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/todos", todosRouter);
+router.use("/generator", generatorRouter);
 router.use(authRouter);
 
 export default router;
